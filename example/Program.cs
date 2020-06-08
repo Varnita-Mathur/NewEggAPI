0f11f6d0a9affbc0abd73785c8dbff7777393232
Varnita Mathur <43050986+Varnita-Mathur@users.noreply.github.com>
2020-06-07 23:49:17 -0400

    Add files via upload

---
+++ example/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Demo demo = new Demo();
+
+            try
+            {
+
+                demo.GetOrderInfo();
+                /*demo.GetOrderStatus();
+                demo.GetSellerStatusCheck();
+                demo.GetAddOrderInfo();
+
+                demo.GetInternationalPrice();
+                demo.UpdateItemlPrice();
+
+                
+                demo.GetSubcategoryProperties();
+                demo.GetSubcategoryStatus();
+
+                demo.SubmitFeed();
+                demo.GetFeedStatus();
+                demo.GetFeedResult();
+
+                demo.GetRMAInfo();
+                demo.GetCourtesyRefundRequestStatus();
+                demo.GetCourtesyRefundInformation();
+
+                demo.SubmitShippingRequest();
+                demo.GetShippingRequestDetail();
+                demo.VoidShippingRequest();
+                demo.ConfirmShippingRequest();
+
+                demo.SubmitDailyInventoryReport();
+                demo.SubmitDailyPriceReport();
+                demo.GetReportStatus();
+                demo.GetDailyInventoryReport();
+
+                demo.VerifyServiceStatus();*/
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exit with error.");
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+            }
+            
+            Console.WriteLine("Exit.");
+            Console.ReadKey();
+        }
+    }
+}
